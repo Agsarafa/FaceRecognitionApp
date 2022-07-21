2ac44f270edf8d335c1e485ba3da37a5b0742692
Andrew Sarafa <andrewsarafa@Andrews-MacBook-Pro.local>
2022-07-20 21:40:42 -0400

    Added alot to this project. Rank, Form, Particles, and fixed running bugs

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import Brain from "./Brain.png";
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
-
-        </div>
+        <Tilt 
+        className="tilt-img"
+        tiltMaxAngleX={35}
+        tiltMaxAngleY={35}
+        perspective={900}
+        scale={1.1}
+        transitionSpeed={2000}
+        gyroscope={true}
+        >
+            <div>
+                <img className="inner-element br2 shadow-2 ma4 mt0 " style={{ height: "100px", width: "100px", paddingTop: "5px"}} alt='logo' src={Brain}></img>
+            </div>
+        </Tilt>
     )
 }
 
